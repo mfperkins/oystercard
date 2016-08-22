4640281708088efd3a6e15f0bc8bba31703b382a
Matthew Perkins <mfperkins@gmail.com>
2016-08-22 14:39:55 +0100

    Step 4 completed: added balance instance variable and tests plus attr_reader

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,4 +2,16 @@ require 'oystercard'
 
 describe Oystercard do
 
+  describe '#initialize' do
+    it 'should set a card to a default balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+
+    it 'should allow user to set a custom balance' do
+      loaded_card = Oystercard.new(50)
+      expect(loaded_card.balance).to eq 50
+    end
+
+  end
+
 end
