144a014aba5c0f2db4468d92abe97a04fb63953c
Andrea Wildsmith <awildsmi@hotmail.com>
2016-08-25 15:34:54 +0100

    14 complete

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,24 +1,37 @@
+require_relative 'journey'
+
 class Oystercard
 
-  attr_reader :balance, :journeys
+  attr_reader :balance, :journeys, :currentjourney
 
   BALANCE_LIMIT = 90
-  MIN_FARE = 1
 
   def initialize
     @balance = 0
     @journeys = {}
+    @currentjourney = nil
   end
 
   def touch_in(station)
-    fail 'you have insufficient funds on your oystercard' if balance < MIN_FARE
+    fail 'you have insufficient funds on your oystercard' if balance < Journey::MIN_FARE
+    if currentjourney != nil
+      deduct(currentjourney.fare)
+      @journey[:entry] = station
+    end
+    @currentjourney = Journey.new
     @journeys[:entry] = station
+    currentjourney.start(station)
   end
 
 
   def touch_out(station)
-    deduct(MIN_FARE)
+    if currentjourney == nil
+      @currentjourney = Journey.new
+    end
+    currentjourney.finish(station)
+    deduct(currentjourney.fare)
     @journeys[:exit] = station
+    @currentjourney = nil
   end
 
   def top_up(value)
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -4,6 +4,7 @@ require 'oystercard.rb'
 describe Oystercard do
   let(:entry_station) {double :station}
   let(:exit_station) {double :station}
+  let(:journey){ {entry: entry_station, exit: exit_station}}
 
   it 'checks default balance of Oystercard is 0' do
     expect(subject.balance).to eq 0
@@ -17,7 +18,7 @@ describe Oystercard do
     expect{ subject.top_up 1 }.to change{ subject.balance }.by 1
   end
 
-  let(:journey){ {entry: entry_station, exit: exit_station}}
+
 
   it 'checks that touching in and out creates one journey' do
     subject.top_up(20)
@@ -39,18 +40,46 @@ describe Oystercard do
 
   describe '#touch_in' do
       let(:station) {double :station}
+      let(:Journey_class) {double :Journey_class, new: journey2}
+      let(:journey2) {double :journey2, start: nil}
+
 
     it 'raises an error if minimum balance on oystercard is less than 1' do
       expect{subject.touch_in(station)}.to raise_error 'you have insufficient funds on your oystercard'
     end
+
+    xit 'send station to journey' do
+      subject.top_up(10)
+      subject.touch_in(station)
+      expect(journey2).to have_received(:start).with(station)
+    end
+
+
   end
+
+
 
   describe '#touch_out' do
 
     it 'updates balance by deducting fare' do
-      subject.top_up(Oystercard::MIN_FARE)
+      subject.top_up(Journey::MIN_FARE)
       subject.touch_in(entry_station)
-      expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by (-Oystercard::MIN_FARE)
+      expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by (-Journey::MIN_FARE)
     end
+
+    it 'creates penalty fare if touch in and haven not touched out' do
+      subject.top_up(20)
+      subject.touch_in(entry_station)
+      expect{ subject.touch_in(entry_station) }.to change{ subject.balance }.by (-Journey::PENALTY_FARE)
+    end
+
+      it 'creates penalty fare if touch out and haven not touched in' do
+        subject.top_up(20)
+        expect{ subject.touch_out(exit_station) }.to change{ subject.balance }.by (-Journey::PENALTY_FARE)
+      end
+
+
+
+    end
+
   end
-end
